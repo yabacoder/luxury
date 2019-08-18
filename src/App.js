4a4b626b7483914b01a5391eb901616e926dc0fa
YabaCoder <yabacoder@gmail.com>
2019-08-18 03:00:13 +0100

    Links sorted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar'
 
 function App() {
-  return <> Hello from App </>;
+  return <> 
+  <Navbar />
+  <Switch>
+  <Route exact path="/" component={Home} />
+  <Route exact path="/rooms/" component={Rooms} />
+  <Route exact path="/rooms/:slug" component={SingleRoom} />
+  <Route component={Error} />
+  </Switch>
+  
+  </>;
 }
 
 export default App;
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>
+            Hello rom room page.
+        </div>
+    )
+}
+
+export default Rooms
